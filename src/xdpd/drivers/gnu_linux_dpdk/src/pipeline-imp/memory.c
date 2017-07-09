15dadf3fd0e122dbd35656499cc0c1d2ff5bcfd3
Marc Sune <marcdevel@gmail.com>
2017-07-09 02:14:47 +0200

    Use cache-aligned structs in DPDK
    Use cache aligned structs in DPDK. Use rte_malloc and rte_free, to
    allocate rofl-pipeline data structures, subject to be used during
    packet processing.
    
    Point rofl-datapath to: cache-aligned commit.

--- src/xdpd/drivers/gnu_linux_dpdk/src/pipeline-imp/memory.c
+++ src/xdpd/drivers/gnu_linux_dpdk/src/pipeline-imp/memory.c
@@ -3,6 +3,7 @@
 
 #include <rofl/datapath/pipeline/platform/memory.h>
 
+#if 0
 /*
  * malloc and free should be replaced if necessary by platform specific memory allocators
  */
@@ -36,7 +37,7 @@ void* platform_memmove( void *dst, const void *src, size_t length ){
 	return memmove( dst, src, length );
 }
 
-#if 0
+#else
 #include <stdlib.h>
 #include <string.h>
 
@@ -45,15 +46,16 @@ void* platform_memmove( void *dst, const void *src, size_t length ){
 #include "../config.h"
 #include <rte_common.h>
 #include <rte_memory.h>
+#include <rte_malloc.h>
 
-
+#define GNU_LINUX_DPDK_ALIGN_MALLOC 64 //Try 0 and 64
 /*
  * malloc and free should be replaced if necessary by platform specific memory allocators
  */
 
 //Per core memory allocators
 void* platform_malloc( size_t length ){
-	return rte_malloc( NULL, length, 0 );
+	return rte_malloc( NULL, length, GNU_LINUX_DPDK_ALIGN_MALLOC );
 }
 void platform_free( void *data ){
 	rte_free( data );
@@ -61,7 +63,7 @@ void platform_free( void *data ){
 
 //Shared memory allocators
 void* platform_malloc_shared( size_t length ){
-	return rte_malloc( NULL, length, 0 );
+	return rte_malloc( NULL, length, GNU_LINUX_DPDK_ALIGN_MALLOC );
 }
 
 void platform_free_shared( void *data ){
